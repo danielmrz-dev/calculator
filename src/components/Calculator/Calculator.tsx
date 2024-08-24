f27f54e2683c6ba9f007e2f8b52df607a6ea759a
Daniel <137360936+danielmrz-dev@users.noreply.github.com>
2024-08-23 22:25:53 -0300

    estrutura dos componentes finalizada

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,6 +1,7 @@
 import * as S from "./StyledCalculator";
 
 export const Calculator: React.FC = () => {
+
     return (
         <S.CalculatorWrapper>
             <S.Header>
@@ -10,19 +11,39 @@ export const Calculator: React.FC = () => {
                         THEME
                     </S.ThemeText>
                     <S.InputsWrapper>
-                        <S.InputLabel>1
-                            <S.ThemeOption type="radio" name="theme"/>
-                        </S.InputLabel>
-                        <S.InputLabel>2
-                            <S.ThemeOption type="radio" name="theme"/>
-                        </S.InputLabel>
-                        <S.InputLabel>3
-                            <S.ThemeOption type="radio" name="theme"/>
-                        </S.InputLabel>
-                    </S.InputsWrapper>
+                        <S.InputLabel htmlFor="one">1</S.InputLabel>
+                        <S.ThemeOption type="radio" name="theme" id="one"/>
 
+                        <S.InputLabel htmlFor="two">2</S.InputLabel>
+                        <S.ThemeOption type="radio" name="theme" id="two"/>
+
+                        <S.InputLabel htmlFor="three">3</S.InputLabel>
+                        <S.ThemeOption type="radio" name="theme" id="three"/>                        
+                    </S.InputsWrapper>
                 </S.ThemeWrapper>
             </S.Header>
+            <S.NumberInput type="number" />
+
+            <S.ButtonsWrapper>
+                <S.Button>7</S.Button>
+                <S.Button>8</S.Button>
+                <S.Button>9</S.Button>
+                <S.Button delOrReset={true}>DEL</S.Button>
+                <S.Button>4</S.Button>
+                <S.Button>5</S.Button>
+                <S.Button>6</S.Button>
+                <S.Button>+</S.Button>
+                <S.Button>1</S.Button>
+                <S.Button>2</S.Button>
+                <S.Button>3</S.Button>
+                <S.Button>-</S.Button>
+                <S.Button>.</S.Button>
+                <S.Button>0</S.Button>
+                <S.Button>/</S.Button>
+                <S.Button>x</S.Button>
+                <S.Button delOrReset={true} twoColumn={true}>RESET</S.Button>
+                <S.Button calculate={true} twoColumn={true}>=</S.Button>
+            </S.ButtonsWrapper>
         </S.CalculatorWrapper>
     )
 }
--- src/components/Calculator/StyledCalculator.ts
+++ src/components/Calculator/StyledCalculator.ts
@@ -1,16 +1,23 @@
 import styled from "styled-components";
 
 export const CalculatorWrapper = styled.main`
-
+    background-color: #ccc;
+    padding: 2rem 1.5rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
 `;
 
 export const Header = styled.header`
     display: flex;
     align-items: center;
+    justify-content: space-between;
 `;
 
 export const Logo = styled.span`
     color: #ffffff;
+    font-weight: 700;
+    font-size: 2rem;
 `;
 
 export const ThemeWrapper = styled.div`
@@ -21,18 +28,89 @@ export const ThemeWrapper = styled.div`
 export const ThemeText = styled.span`
     color: #ffffff;
     align-self: flex-end;
+    letter-spacing: 2px;
+    font-weight: 500;
+    font-size: 0.9rem;
 `;
 
 export const InputsWrapper = styled.div`
-    display: flex;
-
-`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 0.5rem;
+`;
 
 export const InputLabel = styled.label`
     display: flex;
     flex-direction: column;
     align-items: center;
     color: #ffffff;
-`
+    grid-row-start: 1;
+`;
 
-export const ThemeOption = styled.input``;
+export const ThemeOption = styled.input`
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    border: 2px solid #ccc;
+    border-radius: 50%;
+    cursor: pointer;
+    margin-right: 16px; /* Espaçamento extra após o input */
+
+    &:checked {
+        background-color: #4caf50; /* Cor do círculo quando selecionado */
+        border-color: #4caf50;
+    }
+
+    &:hover {
+        border-color: #2196f3; /* Cor do círculo ao passar o mouse */
+    }
+
+    &:focus {
+        outline: none;
+        box-shadow: 0 0 0 3px rgba(33, 150, 243, 0.5); /* Destaque ao focar no input */
+    }
+`;
+
+export const NumberInput = styled.input`
+    width: 100%;
+    padding: 1rem;
+`;
+
+export const ButtonsWrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 0.5rem;
+`;
+
+interface ButtonProps {
+    delOrReset?: boolean;
+    calculate?: boolean;
+    twoColumn?: boolean;
+}
+
+export const Button = styled.button<ButtonProps>`
+    font-size: ${(props) => (props.delOrReset ? "1.5rem" : "2rem")};
+    padding: 1rem 0.75rem 0.75rem;
+    font-weight: 600;
+    border: none;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    grid-column-start: ${(props) => {
+        if (props.delOrReset && props.twoColumn) {
+            return 1;
+        } else if (props.calculate && props.twoColumn) {
+            return 3;
+        }
+        return null;
+    }};
+    grid-column-end: ${(props) => {
+        if (props.delOrReset && props.twoColumn) {
+            return 3;
+        } else if (props.calculate && props.twoColumn) {
+            return 5;
+        }
+        return null;
+    }};
+`;
